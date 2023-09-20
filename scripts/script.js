b61803c491421e05e8a1e4d94142b939531b0e0b
Robin <robin.django@gmail.com>
2023-09-20 12:13:43 +0200

    fixed js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Globally declare some variables that are used in several functions
 var cardsArray1 = [];
 var cardsArray2 = [];
 var cardsArray3 = [];
@@ -7,14 +8,21 @@ const carousel2 = document.querySelector('.carousel-2');
 const carousel3 = document.querySelector('.carousel-3');
 const spinbutton = document.querySelector('.spin-btn');
 
-spinbutton.addEventListener('click', slotmuziek);
+// Add event listener to the spin button
+spinbutton.addEventListener('click', () => {
+  slotmuziek(); 
+  pickRandomCards(
+    cardsArray1,
+    cardsArray2,
+    cardsArray3
+  )}
+);
 
 function slotmuziek() {
   var audiobestand = document.getElementById('slotmuziek');
   audiobestand.play();
 } ;
-
- 
+  
 window.onload = () => {
   // Initialize the card arrays on page load
   cardsArray1 = arrayFromList('.carousel-1 > li');
@@ -51,11 +59,10 @@ const pickRandomCards = async (arr1, arr2, arr3) => {
   selectedCard1.classList.add('selected');
   selectedCard2.classList.add('selected');
   selectedCard3.classList.add('selected');
-  
-  var cardPosition1 = 360/15*findSelectedCardPosition(carousel1, selectedCard1) + 13;
-  var cardPosition2 = 360/15*findSelectedCardPosition(carousel2, selectedCard2) + 13;
-  var cardPosition3 = 360/15*findSelectedCardPosition(carousel3, selectedCard3) + 13;
 
+  var cardPosition1 = 360 / cardsArray1.length * findSelectedCardPosition(carousel1, selectedCard1) + 13;
+  var cardPosition2 = 360 / cardsArray2.length * findSelectedCardPosition(carousel2, selectedCard2) + 13;
+  var cardPosition3 = 360 / cardsArray3.length * findSelectedCardPosition(carousel3, selectedCard3) + 13;
 
   changeSpinState(carousel1, "stop");
   carousel1.style.transform = "translateX(-7.28em) rotateX(" + cardPosition1 + "deg)";
